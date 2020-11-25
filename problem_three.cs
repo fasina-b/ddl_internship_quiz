288b05fa8b502d0e9c74bb6822d16ce1c63fa695
fasina-b <70866105+fasina-b@users.noreply.github.com>
2020-11-25 04:47:03 -0800

    Create problem_three.cs

---
+++ problem_three.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the getMoneySpent function below.
+     */
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) 
+    {
+        
+        int result = 0;
+        int max_value = -1;
+         
+        for (int y = 0; y < keyboards.Length; y++)
+        {
+            for(int x = 0; x < drives.Length; x++)
+            {
+                result = (keyboards[y]) + drives[x];
+                {
+                    if (result > max_value && result <= b)
+                    {
+                        max_value = result;
+                    }
+                }
+            }
+        }
+        return max_value; 
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] bnm = Console.ReadLine().Split(' ');
+
+        int b = Convert.ToInt32(bnm[0]);
+
+        int n = Convert.ToInt32(bnm[1]);
+
+        int m = Convert.ToInt32(bnm[2]);
+
+        int[] keyboards = Array.ConvertAll(Console.ReadLine().Split(' '), keyboardsTemp => Convert.ToInt32(keyboardsTemp))
+        ;
+
+        int[] drives = Array.ConvertAll(Console.ReadLine().Split(' '), drivesTemp => Convert.ToInt32(drivesTemp))
+        ;
+        /*
+         * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+         */
+
+        int moneySpent = getMoneySpent(keyboards, drives, b);
+
+        textWriter.WriteLine(moneySpent);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
